63124751e8113ec7757d639a2526b3da9d47e362
Frank0125 <a01252974@tec.mx>
2024-06-18 20:08:36 -0700

    Inicia

---
+++ Intro_Javascript/main.js
@@ -0,0 +1,20 @@
+// Declaración de variables
+let nombre = "Juan";
+const edad = 25;
+var esEstudiante = true;
+
+// Imprimir en consola
+console.log(nombre);
+console.log(edad);
+console.log(esEstudiante);
+
+// Operaciones básicas
+let suma = 10 + 5;
+let resta = 10 - 5;
+let multiplicacion = 10 * 5;
+let division = 10 / 5;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
